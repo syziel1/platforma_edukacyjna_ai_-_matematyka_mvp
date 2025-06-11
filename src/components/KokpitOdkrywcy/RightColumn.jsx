c3fb19aca244d1c305f863ccb155532f765bdcbe
Sylwester Zieliński <127629548+syziel1@users.noreply.github.com>
2025-06-11 15:16:52 +0200

    Updated RightColumn.jsx

--- src/components/KokpitOdkrywcy/RightColumn.jsx
+++ src/components/KokpitOdkrywcy/RightColumn.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TrainingCard from './TrainingCard';
 import KnowledgeMapCard from './KnowledgeMapCard';
 import AchievementsWidget from './AchievementsWidget';
 
@@ -8,6 +9,7 @@ const RightColumn = ({
 }) => {
   return (
     <div className="space-y-6">
+      <TrainingCard onStartGame={onStartGame} />
       <KnowledgeMapCard onOpenKnowledgeMap={onOpenKnowledgeMap} />
       <AchievementsWidget />
     </div>
