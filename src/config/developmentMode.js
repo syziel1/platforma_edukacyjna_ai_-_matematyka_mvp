46137917312db407574d34280c5c6ed393021027
ZroZoomAI <zrozoomai@gmail.com>
2025-06-14 13:01:10 +0200

    Updated developmentMode.js

--- src/config/developmentMode.js
+++ src/config/developmentMode.js
@@ -1,11 +1,11 @@
 // Development configuration
 export const developmentConfig = {
   // Set to true to show features under construction (even without login)
-  underConstruction: true,
+  underConstruction: false,
   
   // Features that are under construction
   features: {
-    interactiveWhiteboard: true,
+    interactiveWhiteboard: false,
     mentorSessions: true,
     advancedAnalytics: true
   }
