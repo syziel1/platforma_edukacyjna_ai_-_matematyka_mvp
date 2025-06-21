817468cf49b7f3825acf16b504b1265cd88f250d
ZroZoomAI <zrozoomai@gmail.com>
2025-06-21 12:04:05 +0200

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -125,7 +125,7 @@ function App() {
   return (
     <div className="h-screen bg-bg-main flex">
       {/* Fixed Navigation Panel */}
-      <div className="fixed left-0 top-0 bottom-0 z-40">
+      <div /*className="fixed left-0 top-0 bottom-0 z-40"*/>
         <NavigationPanel 
           onLoginClick={() => setShowLogin(true)}
           onShowCockpit={handleShowCockpit}
